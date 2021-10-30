fe43a0ecd72462b1cd1569a3daf6baeb88d8354d
TonyWu20 <tony.w21@gmail.com>
2021-10-30 14:07:00 +0800

    Dactyl manuform

--- keyboards/handwired/dactyl_manuform/4x5/keymaps/tony/config.h
+++ keyboards/handwired/dactyl_manuform/4x5/keymaps/tony/config.h
@@ -23,9 +23,10 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 /* Use I2C or Serial, not both */
 #define USE_SERIAL
 //#define USE_I2C
+#define IGNORE_MOD_TAP_INTERRUPT
 
 /* Select hand configuration */
-//#define MASTER_LEFT
+#define MASTER_LEFT
 //#define MASTER_RIGHT
 
-#define EE_HANDS
+//#define EE_HANDS
