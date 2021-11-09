22fab5d6bc4db877f6287b733d5a17fe73d2ee1d
TonyWu20 <tony.w21@gmail.com>
2021-11-10 03:59:36 +0800

    New keyboard 4x5 cf

---
+++ keyboards/handwired/dactyl_manuform/4x5_cf/keymaps/tony-dm-cf-maltron/keymap.c
@@ -0,0 +1,33 @@
+#include QMK_KEYBOARD_H
+
+#define _BASE 0
+#define _RAISE 1
+#define _LOWER 2
+
+#define SFT_ESC  SFT_T(KC_ESC)
+#define CTL_BSPC CTL_T(KC_BSPC)
+#define ALT_SPC  ALT_T(KC_SPC)
+#define SFT_ENT  SFT_T(KC_ENT)
+
+#define KC_ML KC_MS_LEFT
+#define KC_MR KC_MS_RIGHT
+#define KC_MU KC_MS_UP
+#define KC_MD KC_MS_DOWN
+#define KC_MB1 KC_MS_BTN1
+#define KC_MB2 KC_MS_BTN2
+
+#define RAISE MO(_RAISE)
+#define LOWER MO(_LOWER)
+
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+	[0] = LAYOUT(
+			KC_Q,         KC_P,        KC_Y,  KC_C,         KC_B,                           KC_V, KC_M,         KC_U, KC_Z,         KC_L, 
+			LGUI_T(KC_A), LALT_T(KC_N), KC_I, LCTL_T(KC_S), KC_F,                           KC_D, RCTL_T(KC_T), KC_H, RALT_T(KC_O), RGUI_T(KC_R), 
+			KC_COMM, KC_DOT, KC_J, KC_G, KC_SLSH,                                           KC_SCLN, KC_W, KC_K, KC_MINS, KC_X, 
+			                       KC_J, KC_K,                                              KC_H, KC_L, 
+					       KC_E, LT(1,KC_ESC), KC_LSFT,                             KC_BSPC, LT(2,KC_ENT), RSFT_T(KC_SPC), 
+					       LT(2, KC_NO), KC_TAB, KC_BSPC,                                      KC_CAPS, KC_BSPC, KC_ESC 
+					                                 ),
+	[1] = LAYOUT(KC_AMPR, KC_AT, KC_PERC, KC_TILD, KC_CIRC, KC_DLR, KC_BSLS, KC_MINS, KC_SLSH, KC_HASH, KC_LBRC, KC_LCBR, KC_RCBR, KC_LPRN, KC_EQL, KC_ASTR, KC_RPRN, KC_PLUS, KC_RBRC, KC_EXLM, KC_HOME, KC_END, KC_PGUP, KC_PGDN, KC_TRNS, KC_QUOT, KC_LEFT, KC_UP, KC_DOWN, KC_RGHT, KC_UP, KC_DOWN, KC_LEFT, KC_RGHT, KC_TRNS, KC_TRNS, KC_BTN1, KC_BTN2, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS),
+	[2] = LAYOUT(KC_F10, KC_F7, KC_F8, KC_F9, RESET, KC_PSLS, KC_P7, KC_P8, KC_P9, KC_PPLS, KC_F11, KC_F4, KC_F5, KC_F6, KC_TRNS, KC_PAST, KC_P4, KC_P5, KC_P6, KC_PCMM, KC_F12, KC_F1, KC_F2, KC_F3, KC_TRNS, KC_PMNS, KC_P1, KC_P2, KC_P3, KC_PEQL, KC_VOLU, KC_VOLD, KC_P0, KC_PDOT, KC_MPLY, KC_MSTP, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS)
+};
